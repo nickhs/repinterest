ed325ea0f6d0c365f2166cd2d222e60c6f614643
Nick HS <nickhs@umich.edu>
2012-12-07 20:07:29 -0800

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import run, local, cd, env
+
+env.hosts = ['chicago']
+env.use_ssh_config = True
+
+
+def prepare_deploy():
+    local('git push')
+    print "Pushed changes to github"
+
+
+def deploy():
+    with cd('/srv/repinterest'):
+        run('git pull')
+        run("supervisorctl restart repinterest")
